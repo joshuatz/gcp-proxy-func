85215fa84b747021cf8dabd267d80aed288a7fd2
Joshua Tzucker <joshua.tz@gmail.com>
2019-12-03 14:47:01 -0800

    FIX: Proxying was not working for POSTs with bodies
     * http-proxy-middleware was not passing the POST body along - it was being dropped
     * There are lots of threads on this, but the most helpful was here:
    https://github.com/chimurai/http-proxy-middleware/issues/40#issuecomment-527798602
     * Followed advice and recipe
    (https://github.com/http-party/node-http-proxy/blob/master/examples/middleware/bodyDecoder-middleware.js) to parse body and
    then re-stream in request
     * Tested and working with POST and even application/x-www-form-urlencoded

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 // @ts-check
-
 const express = require('express');
 const config = require('./config');
 const proxy = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
+const queryString = require('querystring');
+
+// Create proxy instance
+const proxyInstance = getProxy();
 
 const app = express();
-app.all('*', getProxy());
+app.use('/', proxyInstance);
+
+// Optional: Support special POST bodies - requires restreaming (see below)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 function getProxy() {
-	return proxy({
+	return proxy(getProxyConfig());
+}
+
+/**
+ * @returns {import('http-proxy-middleware').Config}
+ */
+function getProxyConfig() {
+	return {
 		target: config.destination,
 		changeOrigin: true,
 		followRedirects: true,
-		secure: true
-	});
+		secure: true,
+		onProxyReq: (proxyReq, req, res, options) => {
+			// Restream parsed body before proxying
+			// https://github.com/http-party/node-http-proxy/blob/master/examples/middleware/bodyDecoder-middleware.js
+			if (!req.body || !Object.keys(req.body).length) {
+				return;
+			}
+			const contentType = proxyReq.getHeader('Content-Type');
+			let bodyData;
+			if (contentType === 'application/json') {
+				bodyData = JSON.stringify(req.body);
+			}
+
+			if (contentType === 'application/x-www-form-urlencoded') {
+				bodyData = queryString.stringify(req.body);
+			}
+
+			if (bodyData) {
+				proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+				proxyReq.write(bodyData);
+			}
+		}
+	};
 }
 
 module.exports = {
